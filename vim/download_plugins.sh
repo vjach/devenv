3cef1e218d1e4ca8ca4e5f67e76cefd586ca7975
Victor Jach <ml_dev@gmx.com>
2018-03-15 21:27:44 +0100

    vim config

---
+++ vim/download_plugins.sh
@@ -0,0 +1,9 @@
+# python-mode
+git clone https://github.com/python-mode/python-mode.git
+
+# vim-autoclose
+git clone https://github.com/Townk/vim-autoclose.git
+
+# vim-trailing-whitespace
+git clone https://github.com/bronson/vim-trailing-whitespace.git
+
